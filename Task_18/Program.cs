bed3dc3ed210800405e6b111a8736813657ca23b
George_M <msgoha555@yandex.ru>
2022-05-20 21:18:04 +0300

    Complete Task_18

---
+++ Task_18/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите номер плоскости от 1 до 4: ");
+int n = int.Parse(Console.ReadLine());
+string result = FindXy(n);
+Console.WriteLine(result);
+
+string FindXy(int num)
+{
+    string result = " ";
+    if (num == 1)
+    {
+        result = "Диапозон возможных координат точек в этой четверти равен x и y";
+    }
+    else if (num == 2)
+    {
+        result = "Диапозон возможных координат точек в этой четверти равен x и y";
+    }
+    else if (num == 3)
+    {
+        result = "Диапозон возможных координат точек в этой четверти равен x и y";
+    }
+    else if (num == 4)
+    {
+        result = "Диапозон возможных координат точек в этой четверти равен x и y";
+    }
+    return result;
+}
