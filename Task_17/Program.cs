4fba5cefdecd52135bf70c4d49a2ad4ad937ed24
George_M <msgoha555@yandex.ru>
2022-05-20 20:35:50 +0300

    complete  Task_17

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите значение x: ");
+int x = int.Parse(Console.ReadLine ());
+Console.Write("Введите значение y: ");
+int y = int.Parse(Console.ReadLine ());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1 четверть");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2 четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4 четверть");
+}
