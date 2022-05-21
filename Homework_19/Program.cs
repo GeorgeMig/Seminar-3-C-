7f0e5f9a3579edacdfbcf808fa2585c2b35c1d01
George_M <msgoha555@yandex.ru>
2022-05-21 13:57:40 +0300

    Complete Homework_19

---
+++ Homework_19/Program.cs
@@ -0,0 +1,16 @@
+﻿// ПРОГРАММА, КОТОРАЯ ПРИНИМАЕТ НА ВХОД ПЯТИЗНАЧНОЕ ЧИСЛО И ПРОВЕРЯЕТ, ЯВЛЯЕТСЯ ЛИ ОНО ПАЛИНДРОМОМ
+
+Console.Write("Введите любое пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+int onepartnum = num /1000;
+int twopartnum = FindPalindrom (num);
+Console.WriteLine(onepartnum == twopartnum ? "Да, число является палиндромом" : "Нет, число не является палиндромом");
+
+int FindPalindrom (int number)
+{
+    int twopartnumber = (((number % 100) % 10) * 10) + ((number % 100) / 10);
+    return twopartnumber;
+}
+
+
+// решено черезх метод для закрепления материала, а так можно и без него решить. 
