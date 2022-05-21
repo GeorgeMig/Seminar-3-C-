2a69849d132b9a89ffd57514bd43cc31a7f34558
George_M <msgoha555@yandex.ru>
2022-05-21 12:42:11 +0300

    Complete Task_21

---
+++ Task_21/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите координату х точки А: ");
+int xA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату y точки А: ");
+int yA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату х точки B: ");
+int xB = int.Parse(Console.ReadLine());
+Console.Write("Введите координату y точки B: ");
+int yB = int.Parse(Console.ReadLine());
+
+float dis = getDistance(xA, yA, xB, yB);
+
+float getDistance(int x1, int y1, int x2, int y2)
+{
+    float result = (float)(Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2))));
+    return result;
+}
+
+Console.WriteLine("Расстояние между точками = " + "{0:0.00}", dis);
