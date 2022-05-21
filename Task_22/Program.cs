b1335f54ba08b928964a56f3186220114473a760
George_M <msgoha555@yandex.ru>
2022-05-21 13:19:53 +0300

    Complete Task_22

---
+++ Task_22/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите любое число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+
+Console.WriteLine("|{0,10}|{1,10}|", "Число", "Корень");
+while (count <= num)
+{
+    int square = count * count;
+    Console.WriteLine("|{0,10}|{1,10}|", count, square);
+    count++;
+}
