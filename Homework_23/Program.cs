c1499f1ae2215e6d26f43690029c6706091f5e4b
George_M <msgoha555@yandex.ru>
2022-05-21 14:06:10 +0300

    Complete Homework_23

---
+++ Homework_23/Program.cs
@@ -0,0 +1,13 @@
+﻿//ПРОГРАММА, КОТОРАЯ ПРИНИМАЕТ НА ВХОД ЧИСЛО (N) И ВЫДАЕТ ТАБЛИЦУ КУБОВ ЧИСЕЛ ОТ ! ДО N.
+
+Console.Write("Введите любое число: ");
+double num = double.Parse(Console.ReadLine());
+double count = 1;
+
+Console.WriteLine("|{0,10}|{1,10}|", "Число", "Куб");
+while (count <= num)
+{
+    double square = Math.Pow(count, 3);
+    Console.WriteLine("|{0,10}|{1,10}|", count, square);
+    count++;
+}
